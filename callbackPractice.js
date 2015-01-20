3878ccf25786b47a144c1d9cddea00ee7ebefea6
Stephen Done <srdone@gmail.com>
2015-01-20 07:06:02 -0700

    complete

--- callbackPractice.js
+++ callbackPractice.js
@@ -25,10 +25,13 @@ and what you should write is the favNum function that makes the code above work,
 
 
   //Code Here for first
-  
+var first = function(arr, cb) {
+  cb(arr[0]);
+};
+
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
-  console.log('The first name in names is ', firstName)
+  console.log('The first name in names is ', firstName);
 });
 
 
@@ -39,6 +42,9 @@ first(names, function(firstName){
 
 
   //Code Here for last
+var last = function(arr, cb) {
+  cb(arr[arr.length - 1]);
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 last(names, function(lastName){
@@ -57,10 +63,13 @@ last(names, function(lastName){
 
 
   //Code Here for multiply
+var multiply = function(a, b, cb) {
+  cb(a * b);
+};
 
 multiply(4, 3, function(answer){
   console.log('The answer is ', answer); //should console.log 12
-})
+});
 
 
 
@@ -73,6 +82,9 @@ multiply(4, 3, function(answer){
 
 
   //Code Here for contains
+var contains = function(arr, str, cb) {
+  cb(arr.indexOf(str) !== -1);
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains(names, 'Colt', function(result){
@@ -93,6 +105,13 @@ contains(names, 'Colt', function(result){
 
 
     //Code Here for uniq
+var uniq = function(arr, cb) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (result.indexOf(arr[i]) === -1) result.push(arr[i]);
+  }
+  cb(result);
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 uniq(names, function(uniqArr){
@@ -109,10 +128,15 @@ uniq(names, function(uniqArr){
 
 
     //Code Here for each
+var each = function(arr, cb) {
+  for (var i = 0; i < arr.length; i++) {
+    cb(arr[i], i);
+  }
+};
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item);
 });
 
 
@@ -126,6 +150,14 @@ each(names, function(item, indice){
 
 
  //code here for getUserById
+var getUserById = function(arr, id, cb) {
+  // reduce the array to an object with ids as keys
+  var byId = arr.reduce(function(prev, curr) {
+    prev[curr.id] = curr;
+    return prev;
+  }, {});
+  cb(byId[id]);
+};
 
 var users = [
   {
